32e9fe811b276ba463c6b0a4d2a39231c225e454
COMBE Antoine <antoine.combe@edu.ece.fr>
2018-03-23 13:23:44 +0100

    Init d'Allegro

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <allegro.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int r=255,g=255,b=0;
+    allegro_init();
+    install_mouse();
+    BITMAP *tampon=create_bitmap(800,600);
+    install_keyboard();
+    set_color_depth(desktop_color_depth());
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0)
+    {
+        allegro_message("prb gfx mode");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+
+    while (!key[KEY_ESC])
+    {
+        clear_bitmap(tampon);
+        circlefill(tampon,mouse_x,mouse_y,20,makecol(255,127,127));
+        blit(tampon,screen,0,0,0,0,800,600);
+    }
+
     return 0;
 }
+END_OF_MAIN();
