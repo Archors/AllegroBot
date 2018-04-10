33f6e8c6e45dea32ddb27ddda4cb34a9921f32f8
COMBE Antoine <antoine.combe@edu.ece.fr>
2018-04-10 13:10:35 +0200

    test

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main()
     allegro_init();
     install_mouse();
     BITMAP *tampon=create_bitmap(1280,720);
+    BITMAP *personnage=load_bitmap("Image/Spriterobot2.bmp",NULL);
     install_keyboard();
     set_color_depth(desktop_color_depth());
     if (set_gfx_mode(GFX_AUTODETECT,1280,720,0,0)!=0)
@@ -18,8 +19,9 @@ int main()
     while (!key[KEY_ESC])
     {
         clear_bitmap(tampon);
-        circlefill(tampon,mouse_x,mouse_y,20,makecol(255,127,127));
-
+        blit(personnage,tampon,0,0,0,0,personnage->w,personnage->h);
+        //circlefill(tampon,mouse_x,mouse_y,20,makecol(255,127,127));
+        //masked_blit(personnage,tampon,0,0,mouse_x,mouse_y,60,70);
         blit(tampon,screen,0,0,0,0,1280,720);
     }
     return 0;
